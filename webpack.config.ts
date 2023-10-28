5ce6afd1d68b7dc76ebdf179208f5a19da480ed3
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2023-10-28 18:54:47 +1100

    Setup webpack-dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,7 @@
-module.exports = {
+import { Configuration } from "webpack";
+import "webpack-dev-server";
+
+const config: Configuration = {
     entry: "./frontend/index.ts",
     module: {
         rules: [
@@ -16,4 +19,15 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".js"],
     },
+    mode: "development",
+    devServer: {
+        devMiddleware: {
+            publicPath: "/static/",
+        },
+        proxy: {
+            "/": "http://localhost:8000",
+        },
+    },
 };
+
+export default config;
