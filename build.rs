076d08b5832674e6601518e043f7477a9c1bec93
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2023-11-10 18:44:08 +1100

    Added option to disable running webpack from build.rs

--- build.rs
+++ build.rs
@@ -4,12 +4,16 @@ use std::env;
 use build_pretty::{build_pretty, CommandBuilder};
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=NPM");
+    println!("cargo:rerun-if-env-changed=SKIP_WEBPACK");
     println!("cargo:rerun-if-changed=migrations");
+    if env::var("SKIP_WEBPACK").is_ok() {
+        return;
+    }
     println!("cargo:rerun-if-changed=frontend");
     println!("cargo:rerun-if-changed=package.json");
     println!("cargo:rerun-if-changed=webpack.config.js");
     println!("cargo:rerun-if-changed=tsconfig.json");
+    println!("cargo:rerun-if-env-changed=NPM");
     let mode = match &*env::var("PROFILE").unwrap() {
         "release" => "production",
         "debug" => "development",
